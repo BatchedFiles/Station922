ea30b96933ec6e06c720b970cc9071640641d90d
Эрик Замабувараев‐Ёмолкуу <mabu@sethi.su>
2017-01-13 21:35:24 +0700

    Remove unnecessary lines.

--- make.cmd
+++ make.cmd
@@ -1,7 +1,6 @@
 @echo off
 set ProgramFiles=C:\Program Files (x86)
 "%ProgramFiles%\FreeBASIC\fbc.exe" -r -lib WebServer.bas Network.bas ThreadProc.bas ReadHeadersResult.bas WebUtils.bas ProcessRequests.bas base64-decode.bas Mime.bas Http.bas WebSite.bas
-cscript //Nologo "C:\Programming\FreeBASIC Projects\Test Runtime Library\RemoveLines.vbs" "WebServer.asm" "Network.asm" "ThreadProc.asm" "ThreadProc.asm" "ReadHeadersResult.asm" "WebUtils.asm" "ProcessRequests.asm" "base64-decode.asm" "Mime.asm" "Http.asm" "WebSite.asm"
 "%ProgramFiles%\FreeBASIC\bin\win32\as.exe" --32 --strip-local-absolute "WebServer.asm" -o "WebServer.o"
 "%ProgramFiles%\FreeBASIC\bin\win32\as.exe" --32 --strip-local-absolute "base64-decode.asm" -o "base64-decode.o"
 "%ProgramFiles%\FreeBASIC\bin\win32\as.exe" --32 --strip-local-absolute "Network.asm" -o "Network.o"
@@ -14,8 +13,3 @@ cscript //Nologo "C:\Programming\FreeBASIC Projects\Test Runtime Library\RemoveL
 "%ProgramFiles%\FreeBASIC\bin\win32\as.exe" --32 --strip-local-absolute "WebSite.asm" -o "WebSite.o"
 "%ProgramFiles%\FreeBASIC\bin\win32\gorc" /ni /nw /o /fo "version.obj" "version.rc"
 "%ProgramFiles%\FreeBASIC\bin\win32\ld.exe" -m i386pe -e _EntryPoint@0 -subsystem console -s --stack 1048576,1048576 -L "%programfiles%\freebasic\lib\win32" -L "./" "version.obj" "WebServer.o" "ProcessRequests.o" "WebUtils.o" "Network.o" "ReadHeadersResult.o" "ThreadProc.o" "base64-decode.o" "Mime.o" "Http.o" "WebSite.o" -o "WebServer.exe" -( -lkernel32 -lgdi32 -lmsimg32 -luser32 -lversion -ladvapi32 -limm32 -lshlwapi -lole32 -loleaut32 -lshell32 -lcomctl32 -lws2_32 -lmsvcrt -)
-del *.o *.obj *.asm
-copy /y *.bas "C:\Programming\‘ ©βλ\www.freebasic.su\code\freebasic-webserver\"
-copy /y *.bi "C:\Programming\‘ ©βλ\www.freebasic.su\code\freebasic-webserver\"
-copy /y *.ini "C:\Programming\‘ ©βλ\www.freebasic.su\code\freebasic-webserver\"
-copy /y *.rc "C:\Programming\‘ ©βλ\www.freebasic.su\code\freebasic-webserver\"
