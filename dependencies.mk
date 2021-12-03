dd643418e54f7690a661a200e50bfcc68d6438df
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2021-12-03 16:59:17 +0700

    сценарий компоновщика по объединению секций .xdata и .rdata

---
+++ dependencies.mk
@@ -0,0 +1,364 @@
+$(OBJ_RELEASE_DIR)\ArrayStringWriter$(FILE_SUFFIX_CONSOLE).c: Classes\ArrayStringWriter.bas
+
+$(OBJ_DEBUG_DIR)\ArrayStringWriter$(FILE_SUFFIX_CONSOLE).c:   Classes\ArrayStringWriter.bas
+
+$(OBJ_RELEASE_DIR)\ArrayStringWriter$(FILE_SUFFIX_SERVICE).c: Classes\ArrayStringWriter.bas
+
+$(OBJ_DEBUG_DIR)\ArrayStringWriter$(FILE_SUFFIX_SERVICE).c:   Classes\ArrayStringWriter.bas
+
+$(OBJ_RELEASE_DIR)\ArrayStringWriter$(FILE_SUFFIX_TEST).c: Classes\ArrayStringWriter.bas
+
+$(OBJ_DEBUG_DIR)\ArrayStringWriter$(FILE_SUFFIX_TEST).c:   Classes\ArrayStringWriter.bas
+
+
+$(OBJ_RELEASE_DIR)\AsyncResult$(FILE_SUFFIX_CONSOLE).c: Classes\AsyncResult.bas
+
+$(OBJ_DEBUG_DIR)\AsyncResult$(FILE_SUFFIX_CONSOLE).c:   Classes\AsyncResult.bas
+
+$(OBJ_RELEASE_DIR)\AsyncResult$(FILE_SUFFIX_SERVICE).c: Classes\AsyncResult.bas
+
+$(OBJ_DEBUG_DIR)\AsyncResult$(FILE_SUFFIX_SERVICE).c:   Classes\AsyncResult.bas
+
+$(OBJ_RELEASE_DIR)\AsyncResult$(FILE_SUFFIX_TEST).c: Classes\AsyncResult.bas
+
+$(OBJ_DEBUG_DIR)\AsyncResult$(FILE_SUFFIX_TEST).c:   Classes\AsyncResult.bas
+
+
+$(OBJ_RELEASE_DIR)\ClientContext$(FILE_SUFFIX_CONSOLE).c: Classes\ClientContext.bas
+
+$(OBJ_DEBUG_DIR)\ClientContext$(FILE_SUFFIX_CONSOLE).c:   Classes\ClientContext.bas
+
+$(OBJ_RELEASE_DIR)\ClientContext$(FILE_SUFFIX_SERVICE).c: Classes\ClientContext.bas
+
+$(OBJ_DEBUG_DIR)\ClientContext$(FILE_SUFFIX_SERVICE).c:   Classes\ClientContext.bas
+
+$(OBJ_RELEASE_DIR)\ClientContext$(FILE_SUFFIX_TEST).c: Classes\ClientContext.bas
+
+$(OBJ_DEBUG_DIR)\ClientContext$(FILE_SUFFIX_TEST).c:   Classes\ClientContext.bas
+
+
+$(OBJ_RELEASE_DIR)\ClientRequest$(FILE_SUFFIX_CONSOLE).c: Classes\ClientRequest.bas
+
+$(OBJ_DEBUG_DIR)\ClientRequest$(FILE_SUFFIX_CONSOLE).c:   Classes\ClientRequest.bas
+
+$(OBJ_RELEASE_DIR)\ClientRequest$(FILE_SUFFIX_SERVICE).c: Classes\ClientRequest.bas
+
+$(OBJ_DEBUG_DIR)\ClientRequest$(FILE_SUFFIX_SERVICE).c:   Classes\ClientRequest.bas
+
+$(OBJ_RELEASE_DIR)\ClientRequest$(FILE_SUFFIX_TEST).c: Classes\ClientRequest.bas
+
+$(OBJ_DEBUG_DIR)\ClientRequest$(FILE_SUFFIX_TEST).c:   Classes\ClientRequest.bas
+
+
+$(OBJ_RELEASE_DIR)\ClientUri$(FILE_SUFFIX_CONSOLE).c: Classes\ClientUri.bas
+
+$(OBJ_DEBUG_DIR)\ClientUri$(FILE_SUFFIX_CONSOLE).c:   Classes\ClientUri.bas
+
+$(OBJ_RELEASE_DIR)\ClientUri$(FILE_SUFFIX_SERVICE).c: Classes\ClientUri.bas
+
+$(OBJ_DEBUG_DIR)\ClientUri$(FILE_SUFFIX_SERVICE).c:   Classes\ClientUri.bas
+
+$(OBJ_RELEASE_DIR)\ClientUri$(FILE_SUFFIX_TEST).c: Classes\ClientUri.bas
+
+$(OBJ_DEBUG_DIR)\ClientUri$(FILE_SUFFIX_TEST).c:   Classes\ClientUri.bas
+
+
+$(OBJ_RELEASE_DIR)\CreateInstance$(FILE_SUFFIX_CONSOLE).c: Classes\CreateInstance.bas
+
+$(OBJ_DEBUG_DIR)\CreateInstance$(FILE_SUFFIX_CONSOLE).c:   Classes\CreateInstance.bas
+
+$(OBJ_RELEASE_DIR)\CreateInstance$(FILE_SUFFIX_SERVICE).c: Classes\CreateInstance.bas
+
+$(OBJ_DEBUG_DIR)\CreateInstance$(FILE_SUFFIX_SERVICE).c:   Classes\CreateInstance.bas
+
+$(OBJ_RELEASE_DIR)\CreateInstance$(FILE_SUFFIX_TEST).c: Classes\CreateInstance.bas
+
+$(OBJ_DEBUG_DIR)\CreateInstance$(FILE_SUFFIX_TEST).c:   Classes\CreateInstance.bas
+
+
+$(OBJ_RELEASE_DIR)\HeapBSTR$(FILE_SUFFIX_CONSOLE).c: Classes\HeapBSTR.bas
+
+$(OBJ_DEBUG_DIR)\HeapBSTR$(FILE_SUFFIX_CONSOLE).c:   Classes\HeapBSTR.bas
+
+$(OBJ_RELEASE_DIR)\HeapBSTR$(FILE_SUFFIX_SERVICE).c: Classes\HeapBSTR.bas
+
+$(OBJ_DEBUG_DIR)\HeapBSTR$(FILE_SUFFIX_SERVICE).c:   Classes\HeapBSTR.bas
+
+$(OBJ_RELEASE_DIR)\HeapBSTR$(FILE_SUFFIX_TEST).c: Classes\HeapBSTR.bas
+
+$(OBJ_DEBUG_DIR)\HeapBSTR$(FILE_SUFFIX_TEST).c:   Classes\HeapBSTR.bas
+
+
+$(OBJ_RELEASE_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_CONSOLE).c: Classes\HeapMemoryAllocator.bas
+
+$(OBJ_DEBUG_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_CONSOLE).c:   Classes\HeapMemoryAllocator.bas
+
+$(OBJ_RELEASE_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_SERVICE).c: Classes\HeapMemoryAllocator.bas
+
+$(OBJ_DEBUG_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_SERVICE).c:   Classes\HeapMemoryAllocator.bas
+
+$(OBJ_RELEASE_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_TEST).c: Classes\HeapMemoryAllocator.bas
+
+$(OBJ_DEBUG_DIR)\HeapMemoryAllocator$(FILE_SUFFIX_TEST).c:   Classes\HeapMemoryAllocator.bas
+
+
+$(OBJ_RELEASE_DIR)\HttpGetProcessor$(FILE_SUFFIX_CONSOLE).c: Classes\HttpGetProcessor.bas
+
+$(OBJ_DEBUG_DIR)\HttpGetProcessor$(FILE_SUFFIX_CONSOLE).c:   Classes\HttpGetProcessor.bas
+
+$(OBJ_RELEASE_DIR)\HttpGetProcessor$(FILE_SUFFIX_SERVICE).c: Classes\HttpGetProcessor.bas
+
+$(OBJ_DEBUG_DIR)\HttpGetProcessor$(FILE_SUFFIX_SERVICE).c:   Classes\HttpGetProcessor.bas
+
+$(OBJ_RELEASE_DIR)\HttpGetProcessor$(FILE_SUFFIX_TEST).c: Classes\HttpGetProcessor.bas
+
+$(OBJ_DEBUG_DIR)\HttpGetProcessor$(FILE_SUFFIX_TEST).c:   Classes\HttpGetProcessor.bas
+
+
+$(OBJ_RELEASE_DIR)\HttpReader$(FILE_SUFFIX_CONSOLE).c: Classes\HttpReader.bas
+
+$(OBJ_DEBUG_DIR)\HttpReader$(FILE_SUFFIX_CONSOLE).c:   Classes\HttpReader.bas
+
+$(OBJ_RELEASE_DIR)\HttpReader$(FILE_SUFFIX_SERVICE).c: Classes\HttpReader.bas
+
+$(OBJ_DEBUG_DIR)\HttpReader$(FILE_SUFFIX_SERVICE).c:   Classes\HttpReader.bas
+
+$(OBJ_RELEASE_DIR)\HttpReader$(FILE_SUFFIX_TEST).c: Classes\HttpReader.bas
+
+$(OBJ_DEBUG_DIR)\HttpReader$(FILE_SUFFIX_TEST).c:   Classes\HttpReader.bas
+
+
+$(OBJ_RELEASE_DIR)\NetworkStream$(FILE_SUFFIX_CONSOLE).c: Classes\NetworkStream.bas
+
+$(OBJ_DEBUG_DIR)\NetworkStream$(FILE_SUFFIX_CONSOLE).c:   Classes\NetworkStream.bas
+
+$(OBJ_RELEASE_DIR)\NetworkStream$(FILE_SUFFIX_SERVICE).c: Classes\NetworkStream.bas
+
+$(OBJ_DEBUG_DIR)\NetworkStream$(FILE_SUFFIX_SERVICE).c:   Classes\NetworkStream.bas
+
+$(OBJ_RELEASE_DIR)\NetworkStream$(FILE_SUFFIX_TEST).c: Classes\NetworkStream.bas
+
+$(OBJ_DEBUG_DIR)\NetworkStream$(FILE_SUFFIX_TEST).c:   Classes\NetworkStream.bas
+
+
+$(OBJ_RELEASE_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_CONSOLE).c: Classes\ReadRequestAsyncTask.bas
+
+$(OBJ_DEBUG_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_CONSOLE).c:   Classes\ReadRequestAsyncTask.bas
+
+$(OBJ_RELEASE_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_SERVICE).c: Classes\ReadRequestAsyncTask.bas
+
+$(OBJ_DEBUG_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_SERVICE).c:   Classes\ReadRequestAsyncTask.bas
+
+$(OBJ_RELEASE_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_TEST).c: Classes\ReadRequestAsyncTask.bas
+
+$(OBJ_DEBUG_DIR)\ReadRequestAsyncTask$(FILE_SUFFIX_TEST).c:   Classes\ReadRequestAsyncTask.bas
+
+
+$(OBJ_RELEASE_DIR)\RequestedFile$(FILE_SUFFIX_CONSOLE).c: Classes\RequestedFile.bas
+
+$(OBJ_DEBUG_DIR)\RequestedFile$(FILE_SUFFIX_CONSOLE).c:   Classes\RequestedFile.bas
+
+$(OBJ_RELEASE_DIR)\RequestedFile$(FILE_SUFFIX_SERVICE).c: Classes\RequestedFile.bas
+
+$(OBJ_DEBUG_DIR)\RequestedFile$(FILE_SUFFIX_SERVICE).c:   Classes\RequestedFile.bas
+
+$(OBJ_RELEASE_DIR)\RequestedFile$(FILE_SUFFIX_TEST).c: Classes\RequestedFile.bas
+
+$(OBJ_DEBUG_DIR)\RequestedFile$(FILE_SUFFIX_TEST).c:   Classes\RequestedFile.bas
+
+
+$(OBJ_RELEASE_DIR)\ServerResponse$(FILE_SUFFIX_CONSOLE).c: Classes\ServerResponse.bas
+
+$(OBJ_DEBUG_DIR)\ServerResponse$(FILE_SUFFIX_CONSOLE).c:   Classes\ServerResponse.bas
+
+$(OBJ_RELEASE_DIR)\ServerResponse$(FILE_SUFFIX_SERVICE).c: Classes\ServerResponse.bas
+
+$(OBJ_DEBUG_DIR)\ServerResponse$(FILE_SUFFIX_SERVICE).c:   Classes\ServerResponse.bas
+
+$(OBJ_RELEASE_DIR)\ServerResponse$(FILE_SUFFIX_TEST).c: Classes\ServerResponse.bas
+
+$(OBJ_DEBUG_DIR)\ServerResponse$(FILE_SUFFIX_TEST).c:   Classes\ServerResponse.bas
+
+
+$(OBJ_RELEASE_DIR)\ThreadPool$(FILE_SUFFIX_CONSOLE).c: Classes\ThreadPool.bas
+
+$(OBJ_DEBUG_DIR)\ThreadPool$(FILE_SUFFIX_CONSOLE).c:   Classes\ThreadPool.bas
+
+$(OBJ_RELEASE_DIR)\ThreadPool$(FILE_SUFFIX_SERVICE).c: Classes\ThreadPool.bas
+
+$(OBJ_DEBUG_DIR)\ThreadPool$(FILE_SUFFIX_SERVICE).c:   Classes\ThreadPool.bas
+
+$(OBJ_RELEASE_DIR)\ThreadPool$(FILE_SUFFIX_TEST).c: Classes\ThreadPool.bas
+
+$(OBJ_DEBUG_DIR)\ThreadPool$(FILE_SUFFIX_TEST).c:   Classes\ThreadPool.bas
+
+
+$(OBJ_RELEASE_DIR)\WebServer$(FILE_SUFFIX_CONSOLE).c: Classes\WebServer.bas
+
+$(OBJ_DEBUG_DIR)\WebServer$(FILE_SUFFIX_CONSOLE).c:   Classes\WebServer.bas
+
+$(OBJ_RELEASE_DIR)\WebServer$(FILE_SUFFIX_SERVICE).c: Classes\WebServer.bas
+
+$(OBJ_DEBUG_DIR)\WebServer$(FILE_SUFFIX_SERVICE).c:   Classes\WebServer.bas
+
+$(OBJ_RELEASE_DIR)\WebServer$(FILE_SUFFIX_TEST).c: Classes\WebServer.bas
+
+$(OBJ_DEBUG_DIR)\WebServer$(FILE_SUFFIX_TEST).c:   Classes\WebServer.bas
+
+
+$(OBJ_RELEASE_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_CONSOLE).c: Classes\WebServerIniConfiguration.bas
+
+$(OBJ_DEBUG_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_CONSOLE).c:   Classes\WebServerIniConfiguration.bas
+
+$(OBJ_RELEASE_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_SERVICE).c: Classes\WebServerIniConfiguration.bas
+
+$(OBJ_DEBUG_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_SERVICE).c:   Classes\WebServerIniConfiguration.bas
+
+$(OBJ_RELEASE_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_TEST).c: Classes\WebServerIniConfiguration.bas
+
+$(OBJ_DEBUG_DIR)\WebServerIniConfiguration$(FILE_SUFFIX_TEST).c:   Classes\WebServerIniConfiguration.bas
+
+
+$(OBJ_RELEASE_DIR)\WebSite$(FILE_SUFFIX_CONSOLE).c: Classes\WebSite.bas
+
+$(OBJ_DEBUG_DIR)\WebSite$(FILE_SUFFIX_CONSOLE).c:   Classes\WebSite.bas
+
+$(OBJ_RELEASE_DIR)\WebSite$(FILE_SUFFIX_SERVICE).c: Classes\WebSite.bas
+
+$(OBJ_DEBUG_DIR)\WebSite$(FILE_SUFFIX_SERVICE).c:   Classes\WebSite.bas
+
+$(OBJ_RELEASE_DIR)\WebSite$(FILE_SUFFIX_TEST).c: Classes\WebSite.bas
+
+$(OBJ_DEBUG_DIR)\WebSite$(FILE_SUFFIX_TEST).c:   Classes\WebSite.bas
+
+
+$(OBJ_RELEASE_DIR)\WebSiteCollection$(FILE_SUFFIX_CONSOLE).c: Classes\WebSiteCollection.bas
+
+$(OBJ_DEBUG_DIR)\WebSiteCollection$(FILE_SUFFIX_CONSOLE).c:   Classes\WebSiteCollection.bas
+
+$(OBJ_RELEASE_DIR)\WebSiteCollection$(FILE_SUFFIX_SERVICE).c: Classes\WebSiteCollection.bas
+
+$(OBJ_DEBUG_DIR)\WebSiteCollection$(FILE_SUFFIX_SERVICE).c:   Classes\WebSiteCollection.bas
+
+$(OBJ_RELEASE_DIR)\WebSiteCollection$(FILE_SUFFIX_TEST).c: Classes\WebSiteCollection.bas
+
+$(OBJ_DEBUG_DIR)\WebSiteCollection$(FILE_SUFFIX_TEST).c:   Classes\WebSiteCollection.bas
+
+
+$(OBJ_RELEASE_DIR)\ConsoleMain$(FILE_SUFFIX_CONSOLE).c: Modules\ConsoleMain.bas
+
+$(OBJ_DEBUG_DIR)\ConsoleMain$(FILE_SUFFIX_CONSOLE).c:   Modules\ConsoleMain.bas
+
+
+$(OBJ_RELEASE_DIR)\Http$(FILE_SUFFIX_CONSOLE).c: Modules\Http.bas
+
+$(OBJ_DEBUG_DIR)\Http$(FILE_SUFFIX_CONSOLE).c:   Modules\Http.bas
+
+$(OBJ_RELEASE_DIR)\Http$(FILE_SUFFIX_SERVICE).c: Modules\Http.bas
+
+$(OBJ_DEBUG_DIR)\Http$(FILE_SUFFIX_SERVICE).c:   Modules\Http.bas
+
+$(OBJ_RELEASE_DIR)\Http$(FILE_SUFFIX_TEST).c: Modules\Http.bas
+
+$(OBJ_DEBUG_DIR)\Http$(FILE_SUFFIX_TEST).c:   Modules\Http.bas
+
+
+$(OBJ_RELEASE_DIR)\Logger$(FILE_SUFFIX_CONSOLE).c: Modules\Logger.bas
+
+$(OBJ_DEBUG_DIR)\Logger$(FILE_SUFFIX_CONSOLE).c:   Modules\Logger.bas
+
+$(OBJ_RELEASE_DIR)\Logger$(FILE_SUFFIX_SERVICE).c: Modules\Logger.bas
+
+$(OBJ_DEBUG_DIR)\Logger$(FILE_SUFFIX_SERVICE).c:   Modules\Logger.bas
+
+$(OBJ_RELEASE_DIR)\Logger$(FILE_SUFFIX_TEST).c: Modules\Logger.bas
+
+$(OBJ_DEBUG_DIR)\Logger$(FILE_SUFFIX_TEST).c:   Modules\Logger.bas
+
+
+$(OBJ_RELEASE_DIR)\Mime$(FILE_SUFFIX_CONSOLE).c: Modules\Mime.bas
+
+$(OBJ_DEBUG_DIR)\Mime$(FILE_SUFFIX_CONSOLE).c:   Modules\Mime.bas
+
+$(OBJ_RELEASE_DIR)\Mime$(FILE_SUFFIX_SERVICE).c: Modules\Mime.bas
+
+$(OBJ_DEBUG_DIR)\Mime$(FILE_SUFFIX_SERVICE).c:   Modules\Mime.bas
+
+$(OBJ_RELEASE_DIR)\Mime$(FILE_SUFFIX_TEST).c: Modules\Mime.bas
+
+$(OBJ_DEBUG_DIR)\Mime$(FILE_SUFFIX_TEST).c:   Modules\Mime.bas
+
+
+$(OBJ_RELEASE_DIR)\Network$(FILE_SUFFIX_CONSOLE).c: Modules\Network.bas
+
+$(OBJ_DEBUG_DIR)\Network$(FILE_SUFFIX_CONSOLE).c:   Modules\Network.bas
+
+$(OBJ_RELEASE_DIR)\Network$(FILE_SUFFIX_SERVICE).c: Modules\Network.bas
+
+$(OBJ_DEBUG_DIR)\Network$(FILE_SUFFIX_SERVICE).c:   Modules\Network.bas
+
+$(OBJ_RELEASE_DIR)\Network$(FILE_SUFFIX_TEST).c: Modules\Network.bas
+
+$(OBJ_DEBUG_DIR)\Network$(FILE_SUFFIX_TEST).c:   Modules\Network.bas
+
+
+$(OBJ_RELEASE_DIR)\SafeHandle$(FILE_SUFFIX_CONSOLE).c: Modules\SafeHandle.bas
+
+$(OBJ_DEBUG_DIR)\SafeHandle$(FILE_SUFFIX_CONSOLE).c:   Modules\SafeHandle.bas
+
+$(OBJ_RELEASE_DIR)\SafeHandle$(FILE_SUFFIX_SERVICE).c: Modules\SafeHandle.bas
+
+$(OBJ_DEBUG_DIR)\SafeHandle$(FILE_SUFFIX_SERVICE).c:   Modules\SafeHandle.bas
+
+$(OBJ_RELEASE_DIR)\SafeHandle$(FILE_SUFFIX_TEST).c: Modules\SafeHandle.bas
+
+$(OBJ_DEBUG_DIR)\SafeHandle$(FILE_SUFFIX_TEST).c:   Modules\SafeHandle.bas
+
+
+$(OBJ_RELEASE_DIR)\TestMain$(FILE_SUFFIX_TEST).c: Modules\TestMain.bas
+
+$(OBJ_DEBUG_DIR)\TestMain$(FILE_SUFFIX_TEST).c:   Modules\TestMain.bas
+
+
+$(OBJ_RELEASE_DIR)\WebUtils$(FILE_SUFFIX_CONSOLE).c: Modules\WebUtils.bas
+
+$(OBJ_DEBUG_DIR)\WebUtils$(FILE_SUFFIX_CONSOLE).c:   Modules\WebUtils.bas
+
+$(OBJ_RELEASE_DIR)\WebUtils$(FILE_SUFFIX_SERVICE).c: Modules\WebUtils.bas
+
+$(OBJ_DEBUG_DIR)\WebUtils$(FILE_SUFFIX_SERVICE).c:   Modules\WebUtils.bas
+
+$(OBJ_RELEASE_DIR)\WebUtils$(FILE_SUFFIX_TEST).c: Modules\WebUtils.bas
+
+$(OBJ_DEBUG_DIR)\WebUtils$(FILE_SUFFIX_TEST).c:   Modules\WebUtils.bas
+
+
+$(OBJ_RELEASE_DIR)\WindowsServiceMain$(FILE_SUFFIX_SERVICE).c: Modules\WindowsServiceMain.bas
+
+$(OBJ_DEBUG_DIR)\WindowsServiceMain$(FILE_SUFFIX_SERVICE).c:   Modules\WindowsServiceMain.bas
+
+
+$(OBJ_RELEASE_DIR)\WriteHttpError$(FILE_SUFFIX_CONSOLE).c: Modules\WriteHttpError.bas
+
+$(OBJ_DEBUG_DIR)\WriteHttpError$(FILE_SUFFIX_CONSOLE).c:   Modules\WriteHttpError.bas
+
+$(OBJ_RELEASE_DIR)\WriteHttpError$(FILE_SUFFIX_SERVICE).c: Modules\WriteHttpError.bas
+
+$(OBJ_DEBUG_DIR)\WriteHttpError$(FILE_SUFFIX_SERVICE).c:   Modules\WriteHttpError.bas
+
+$(OBJ_RELEASE_DIR)\WriteHttpError$(FILE_SUFFIX_TEST).c: Modules\WriteHttpError.bas
+
+$(OBJ_DEBUG_DIR)\WriteHttpError$(FILE_SUFFIX_TEST).c:   Modules\WriteHttpError.bas
+
+
+$(OBJ_RELEASE_DIR)\EntryPoint$(FILE_SUFFIX_CONSOLE).c: EntryPoint.bas
+
+$(OBJ_DEBUG_DIR)\EntryPoint$(FILE_SUFFIX_CONSOLE).c:   EntryPoint.bas
+
+$(OBJ_RELEASE_DIR)\EntryPoint$(FILE_SUFFIX_SERVICE).c: EntryPoint.bas
+
+$(OBJ_DEBUG_DIR)\EntryPoint$(FILE_SUFFIX_SERVICE).c:   EntryPoint.bas
+
+$(OBJ_RELEASE_DIR)\EntryPoint$(FILE_SUFFIX_TEST).c: EntryPoint.bas
+
+$(OBJ_DEBUG_DIR)\EntryPoint$(FILE_SUFFIX_TEST).c:   EntryPoint.bas
